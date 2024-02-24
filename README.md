31a7527b09ae91158f0735b6aa3b1980beecc0ff
Arnav Ghosh <arnavghosh.official.2004@gmail.com>
2024-02-24 22:47:54 +0530

    Added License and README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Veena
+A Simple Music Player (SPA) that uses the [YT Music Relay API](https://ytmrelay-api.onrender.com/).
+This is a Proof of Concept or an Eample o how to se the API. Any feature added the API will be Tested here.
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,8 @@
-import './style.css'
-import './main.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
 import Alpine from 'alpinejs'
-import { setupCounter } from './counter.ts'
+import component from 'alpinejs-component'
+
+import './main.css'
 
 window.Alpine = Alpine
-Alpine.start()
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button" class="p-2 rounded-lg bg-red-300 hover:bg-red-400 text-black"></button>
-    </div>
-    <p class="read-the-docs p-2">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+Alpine.plugin(component)
+Alpine.start()// import { setupCounter } from './counter.ts'
